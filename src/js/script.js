35d5fefa6d9d03dbc066daba2753a7e76de89a95
David Ponce <buap.david@gmail.com>
2021-09-22 22:24:54 -0500

    release(Root): Optimize load to resource of skills sections

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,31 @@
-const globalNavbar = document.querySelector("#navbar");
-const landingNavbar = document.querySelector(".landing__nav");
+const globalNavbar = document.querySelector('#navbar')
+const landingNavbar = document.querySelector('.landing__nav')
 
-const landingSection = document.querySelector(".landing");
-const aboutMeSection = document.querySelector("#about-me");
-const skillsSection = document.querySelector("#skills");
-const projectsSection = document.querySelector("#projects");
+const landingSection = document.querySelector('.landing')
+const aboutMeSection = document.querySelector('#about-me')
+const skillsSection = document.querySelector('#skills')
+const projectsSection = document.querySelector('#projects')
 
-function onChangeNavbar(entries) {
-  const [entry] = entries;
+function onChangeNavbar (entries) {
+  const [entry] = entries
 
   entry.isIntersecting
-    ? globalNavbar.classList.add("navbar--hidden")
-    : globalNavbar.classList.remove("navbar--hidden");
+    ? globalNavbar.classList.add('navbar--hidden')
+    : globalNavbar.classList.remove('navbar--hidden')
 }
 
-function onChangeSection(entries) {
-  entries.map((entry) => {
+function onChangeSection (entries) {
+  entries.forEach((entry) => {
     const newURL = entry.target.id ? `#${entry.target.id}` : '/'
     if (entry.isIntersecting) window.history.pushState(null, 'title', newURL)
   })
 }
 
-const observerToNavbar = new IntersectionObserver(onChangeNavbar, { threshold: 1.0 });
-const observerToSections = new IntersectionObserver(onChangeSection, {threshold: 0.5});
+const observerToNavbar = new window.IntersectionObserver(onChangeNavbar, { threshold: 1.0 })
+const observerToSections = new window.IntersectionObserver(onChangeSection, { threshold: 0.5 })
 
-observerToNavbar.observe(landingNavbar);
-observerToSections.observe(landingSection);
-observerToSections.observe(aboutMeSection);
-observerToSections.observe(skillsSection);
-observerToSections.observe(projectsSection);
-
+observerToNavbar.observe(landingNavbar)
+observerToSections.observe(landingSection)
+observerToSections.observe(aboutMeSection)
+observerToSections.observe(skillsSection)
+observerToSections.observe(projectsSection)
